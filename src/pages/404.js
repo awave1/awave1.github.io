6b81ef64474855884ae23feab97ba499ab35be84
Artem Golovin <artgolovin96@gmail.com>
2020-06-24 12:00:37 -0600

    fix: 🐛 import correct Layout in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
     <small
       style={{
-        margin: '0 auto',
-        alignSelf: 'center',
-        fontFamily: 'Hack, monospace',
-        letterSpacing: '10px',
+        margin: "0 auto",
+        alignSelf: "center",
+        fontFamily: "Hack, monospace",
+        letterSpacing: "10px",
       }}
     >
       emptyspace
