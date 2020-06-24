139d828c0af92c5fa3c4dc6677235ec057337df9
Artem Golovin <artgolovin96@gmail.com>
2020-06-24 12:14:21 -0600

    fix: 🐛 remove unused Toolbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import { Toolbar } from "./components/Toolbar";
 import Home from "./pages/index";
 import About from "./pages/about";
 import Work from "./pages/work";
