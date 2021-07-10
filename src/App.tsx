0928a2f755235ded37fe2761bbaeb3e27a51de26
awave1 <artgolovin96@gmail.com>
2021-07-09 23:52:19 -0600

    feat: switching to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import Home from "./pages/index";
-import About from "./pages/about";
-import Work from "./pages/work";
-
-import "./layout.css";
-import "./components/Waves/waves.css";
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <Router>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"
-        />
-        <title>some wavy dude</title>
-      </Helmet>
+  const [count, setCount] = useState(0)
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/work">
-          <Work />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Waves/types.ts
+++
@@ -1,3 +0,0 @@
-export type WaveEmojiProps = {
-  className?: string;
-};
